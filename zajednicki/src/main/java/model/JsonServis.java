d83d3302883532ebf60acdb9a3851aad65156594
tijana-milosavljevic <tmilosavljevic132@gmail.com>
2026-06-15 22:09:00 +0200

    Refaktorisan JsonServis i dodata rucna serijalizacija

--- zajednicki/src/main/java/model/JsonServis.java
+++ zajednicki/src/main/java/model/JsonServis.java
@@ -2,6 +2,8 @@ package model;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -13,49 +15,74 @@ public class JsonServis {
 
     public void serijalizujInstruktore(List<Instruktor> instruktori, String putanja) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
-        FileWriter writer = new FileWriter(putanja);
-        writer.write(gson.toJson(instruktori));
-        writer.close();
+        try (FileWriter writer = new FileWriter(putanja)) {
+            writer.write(gson.toJson(instruktori));
+        }
     }
 
     public List<Instruktor> deserijalizujInstruktore(String putanja) throws IOException {
         Gson gson = new GsonBuilder().create();
-        FileReader reader = new FileReader(putanja);
         Type listType = new TypeToken<List<Instruktor>>(){}.getType();
-        List<Instruktor> instruktori = gson.fromJson(reader, listType);
-        reader.close();
-        return instruktori;
+        try (FileReader reader = new FileReader(putanja)) {
+            return gson.fromJson(reader, listType);
+        }
     }
-    
+
     public void serijalizujPolaznike(List<Polaznik> polaznici, String putanja) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
-        FileWriter writer = new FileWriter(putanja);
-        writer.write(gson.toJson(polaznici));
-        writer.close();
+        try (FileWriter writer = new FileWriter(putanja)) {
+            writer.write(gson.toJson(polaznici));
+        }
     }
 
     public List<Polaznik> deserijalizujPolaznike(String putanja) throws IOException {
         Gson gson = new GsonBuilder().create();
-        FileReader reader = new FileReader(putanja);
         Type listType = new TypeToken<List<Polaznik>>(){}.getType();
-        List<Polaznik> polaznici = gson.fromJson(reader, listType);
-        reader.close();
-        return polaznici;
+        try (FileReader reader = new FileReader(putanja)) {
+            return gson.fromJson(reader, listType);
+        }
     }
 
     public void serijalizujUpisnice(List<Upisnica> upisnice, String putanja) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
-        FileWriter writer = new FileWriter(putanja);
-        writer.write(gson.toJson(upisnice));
-        writer.close();
+        try (FileWriter writer = new FileWriter(putanja)) {
+            writer.write(gson.toJson(upisnice));
+        }
     }
 
     public List<Upisnica> deserijalizujUpisnice(String putanja) throws IOException {
         Gson gson = new GsonBuilder().create();
-        FileReader reader = new FileReader(putanja);
         Type listType = new TypeToken<List<Upisnica>>(){}.getType();
-        List<Upisnica> upisnice = gson.fromJson(reader, listType);
-        reader.close();
-        return upisnice;
+        try (FileReader reader = new FileReader(putanja)) {
+            return gson.fromJson(reader, listType);
+        }
+    }
+
+    public void serijalizujInstruktoreRucno(List<Instruktor> instruktori, String putanja) throws IOException {
+        JsonArray niz = new JsonArray();
+
+        for (Instruktor i : instruktori) {
+            JsonObject obj = new JsonObject();
+            obj.addProperty("ime", i.getIme());
+            obj.addProperty("prezime", i.getPrezime());
+            obj.addProperty("email", i.getEmail());
+
+            JsonArray kvalifikacije = new JsonArray();
+            if (i.getInstruktorKvalifikacije() != null) {
+                for (InstruktorKvalifikacija ik : i.getInstruktorKvalifikacije()) {
+                    JsonObject kvalObj = new JsonObject();
+                    kvalObj.addProperty("tip", ik.getKvalifikacija().getTip());
+                    kvalObj.addProperty("organizacija", ik.getKvalifikacija().getOrganizacija());
+                    kvalObj.addProperty("nivo", ik.getNivo().toString());
+                    kvalifikacije.add(kvalObj);
+                }
+            }
+            obj.add("kvalifikacije", kvalifikacije);
+            niz.add(obj);
+        }
+
+        try (FileWriter writer = new FileWriter(putanja)) {
+            writer.write(new GsonBuilder().setPrettyPrinting().create().toJson(niz));
+        }
     }
 }
